779ce7e3501f63077049a39453640047c7bde93f
elppans <elppansmk@gmail.com>
2026-07-29 10:31:31 -0300

    Enable locale generation for pt_BR ISO-8859-1
    Add locale generation for pt_BR ISO-8859-1 if enabled

--- psql-locale.sh
+++ psql-locale.sh
@@ -12,13 +12,21 @@ fi
 
 if grep '^# pt_BR ISO-8859-1' /etc/locale.gen ; then
 	sudo sed -i 's/^# *pt_BR ISO-8859-1/pt_BR ISO-8859-1/' /etc/locale.gen
+	LOGEN="1"
 	PSQLRST="1"
+	export LOGEN
 	export PSQLRST
 fi
 if grep '^# pt_BR ISO-8859-1' /etc/locale.alias ; then
 	sudo sed -i 's/^# *pt_BR ISO-8859-1/pt_BR ISO-8859-1/' /etc/locale.alias
+	LOGEN="1"
 	PSQLRST="1"
+	export LOGEN
 	export PSQLRST
+fi
+
+if [ "$LOGEN" == "1" ]; then
+	sudo locale-gen
 fi
 
 if [ "$PSQLRST" == "1" ]; then
