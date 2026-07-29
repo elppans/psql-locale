42081b77c0c433172c1546d3cdc5bfd5dc981eee
elppans <elppansmk@gmail.com>
2026-07-29 10:22:51 -0300

    Implement locale verification and cron job setup
    Add cron job to verify pt_BR ISO-8859-1 locale every NN minutes and restart PostgreSQL if necessary.

---
+++ psql-locale.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if grep '55/* *	* * *	root  /usr/local/bin/psql-locale.sh' /etc/crontab ; then
+sudo tee -a /etc/crontab &>>/dev/null <<'EOF'
+
+# Verifcar locale pt_BR ISO-8859-1 no sistema a cada NN Minutos
+55/* *	* * *	root  /usr/local/bin/psql-locale.sh' /etc/crontab
+
+EOF
+fi
+
+if grep '^# pt_BR ISO-8859-1' /etc/locale.gen ; then
+	sudo sed -i 's/^# *pt_BR ISO-8859-1/pt_BR ISO-8859-1/' /etc/locale.gen
+	PSQLRST="1"
+	export PSQLRST
+fi
+if grep '^# pt_BR ISO-8859-1' /etc/locale.alias ; then
+	sudo sed -i 's/^# *pt_BR ISO-8859-1/pt_BR ISO-8859-1/' /etc/locale.alias
+	PSQLRST="1"
+	export PSQLRST
+fi
+
+if [ "$PSQLRST" == "1" ]; then
+	sudo systemctl restart postgresql@14-main.service
+fi
+
+
